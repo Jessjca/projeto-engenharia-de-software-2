885f4ec5de3bc80c1d93af6b086df421bc9bb76b
Mateus <mateus.vitor.andrade@gmail.com>
2025-03-17 11:03:05 -0300

    add controllers to the project

---
+++ src/Controller/LoginController.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+
+import Controller.Helper.LoginHelper;
+import View.Login;
+import model.Usuario;
+
+/**
+ *
+ * @author Mateus Andrade
+ */
+public class LoginController {
+
+    private final Login view;
+    private LoginHelper helper;
+
+    public LoginController(Login view) {
+        this.view = view;
+        this.helper = new LoginHelper(view);
+        
+    }
+    
+    public void entrarSistema(){
+        Usuario usuario = helper.obterModelo();
+        
+
+        
+    }
+    
+    public void fizTarefa(){
+        System.out.println("Busquei algo do banco de dados");
+        this.view.exibeMensagem("Executei o Fiz Tarefa");
+    }
+    
+    
+}
